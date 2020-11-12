81a8836b64af06a01b84e36825aba1a2011d0cdf
obult <oswin.bult@live.nl>
2020-11-12 12:01:11 +0100

    new split type

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: obult <obult@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/03 22:19:32 by obult         #+#    #+#                 */
-/*   Updated: 2020/11/03 22:19:33 by obult         ########   odam.nl         */
+/*   Updated: 2020/11/10 13:55:52 by obult         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@ static void	ft_putdig_fd(int dig, int fd)
 void		ft_putnbr_fd(int n, int fd)
 {
 	if (n < 10 && n >= 0)
-	{
 		ft_putdig_fd(n, fd);
-	}
 	else if (n > 0)
 	{
 		ft_putnbr_fd(n / 10, fd);
