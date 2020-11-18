9b3a88a7423321fb2608ab6bd57788816820767b
Oswin Bult <oswin.bult@gmail.com>
2020-11-18 15:20:31 +0100

    a new simpler strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,40 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   ft_strnstr.c                                       :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: obult <obult@student.codam.nl>               +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2020/11/03 22:22:19 by obult         #+#    #+#                 */
-/*   Updated: 2020/11/13 23:59:14 by obult         ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
+#include <stdio.h>
 #include <stdlib.h>
-#include "libft.h"
 
-static int	ft_compare(unsigned char *big, unsigned char *tiny)
-{
-	if (!(*tiny))
-		return (1);
-	if (*tiny != *big)
-		return (0);
-	return (ft_compare(big + 1, tiny + 1));
-}
-
-char		*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *hay, const char *needle, size_t len)
 {
 	size_t	i;
-	size_t	l_len;
+	size_t	j;
 
 	i = 0;
-	l_len = ft_strlen(little);
-	while (i <= len - l_len && big[i] && l_len <= len)
+	while (hay[i] && i < len)
 	{
-		if (ft_compare((unsigned char *)&big[i], (unsigned char *)little))
-		{
-			return ((char *)&big[i]);
-		}
+		j = 0;
+		while (needle[j] && needle[j] == hay[i + j] && i + j < len)
+			j++;
+		if (!needle[j])
+			return ((char *)&hay[i]);
 		i++;
 	}
 	return (0);
