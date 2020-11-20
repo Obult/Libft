1df29fa1ef399d5fa9d4f59509b8805e4603445a
Oswin Bult <oswin.bult@gmail.com>
2020-11-21 00:04:38 +0100

    a new memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: obult <obult@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/03 22:21:04 by obult         #+#    #+#                 */
-/*   Updated: 2020/11/20 11:43:39 by oswin         ########   odam.nl         */
+/*   Updated: 2020/11/21 00:02:36 by oswin         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (((unsigned char *)s)[i] == c)
-			return (&((unsigned char *)s)[i]);
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return ((void *)&((unsigned char *)s)[i]);
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
